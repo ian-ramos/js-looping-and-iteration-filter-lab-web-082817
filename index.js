93ab7d394939def4279685460e6974b01090c7cc
ian-ramos <ramos.ianc@gmail.com>
2017-10-09 11:43:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(function (n) { return n.toLowerCase() === name.toLowerCase() });
+}
+
+function fuzzyMatch(array, letters) {
+  return array.filter(function (n) { return n.startsWith(letters) });
+}
+
+function matchName(array, string) {
+  return array.filter(function (n) { return n.name === string });
+}
